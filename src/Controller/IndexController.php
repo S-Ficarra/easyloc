ee37793d773d7ddb2e79a71d7ceffce5eebc92b2
Sylvain Ficarra <contact@ficarra.fr>
2023-12-04 11:40:08 +0100

    feat: index page added with navigation

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class indexController extends AbstractController {
+
+
+    public function indexPage (Request $request) : Response 
+    {
+        return $this->render('index.html.twig');
+    } 
+}
